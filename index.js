502cb7bab62590716d5f4c21c5d544492d3bc08e
vynZGaming <74480800+vynZGaming@users.noreply.github.com>
2021-10-27 20:08:56 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8080 || 5000 || 3000
 var { color } = require('./lib/color.js')
 
 var mainrouter = require('./routes/main')
-
+    
 var app = express()
 app.enable('trust proxy');
 app.set("json spaces",2)
@@ -14,6 +14,7 @@ app.use(secure)
 app.use(express.static("public"))
 
 app.use('/', mainrouter)
+
 
 app.listen(PORT, () => {
     console.log(color("Server running on port " + PORT,'green'))
